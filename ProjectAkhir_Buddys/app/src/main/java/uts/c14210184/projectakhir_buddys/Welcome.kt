a89e523967885973d4265b84875619f4758c1a62
Frederika <c14210185@john.petra.ac.id>
2023-12-20 13:27:53 +0700

    profile, edit profile, welcome

---
+++ ProjectAkhir_Buddys/app/src/main/java/uts/c14210184/projectakhir_buddys/Welcome.kt
@@ -0,0 +1,11 @@
+package uts.c14210184.projectakhir_buddys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
