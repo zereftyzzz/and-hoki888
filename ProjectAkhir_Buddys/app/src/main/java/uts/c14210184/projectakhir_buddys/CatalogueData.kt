4d1859d9a2ee7a3ee7e7b822e30fdb52c4aa5120
zereftyzzz <c14210184@john.petra.ac.id>
2023-12-21 19:20:46 +0700

    nambahin data class

---
+++ ProjectAkhir_Buddys/app/src/main/java/uts/c14210184/projectakhir_buddys/CatalogueData.kt
@@ -0,0 +1,7 @@
+package uts.c14210184.projectakhir_buddys
+
+data class CatalogueData(
+    var gambar: String,
+    var item: String,
+    var categories: String
+)
---
+++ ProjectAkhir_Buddys/app/src/main/java/uts/c14210184/projectakhir_buddys/MyPostData.kt
@@ -0,0 +1,9 @@
+package uts.c14210184.projectakhir_buddys
+
+data class MyPostData(
+    var title : String,
+    var description : String,
+    var author: String,
+    var image: String,
+    var view: Int
+)
