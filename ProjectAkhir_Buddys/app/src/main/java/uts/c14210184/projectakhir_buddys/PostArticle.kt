5a66280c57fd5986508c89a884aaca9360c3c416
Frederika <c14210185@john.petra.ac.id>
2023-12-21 18:39:18 +0700

    tambah dan read data firebase, kurang tampilkan di tv dan delete

--- ProjectAkhir_Buddys/app/src/main/java/uts/c14210184/projectakhir_buddys/PostArticle.kt
+++ ProjectAkhir_Buddys/app/src/main/java/uts/c14210184/projectakhir_buddys/PostArticle.kt
@@ -1,13 +1,20 @@
 package uts.c14210184.projectakhir_buddys
 
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import kotlin.random.Random
 import androidx.fragment.app.FragmentManager
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,22 +31,31 @@ class PostArticle : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    val db = Firebase.firestore
+    lateinit var _etTitle : EditText
+    lateinit var _etDesc : EditText
+    lateinit var _etAuthor : EditText
+    lateinit var _etImage : EditText
 
-
-    }
+    var dataArticle = ArrayList<ArticleData>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //        Kembali ke Fragment Article
+        //        Kembali ke Fragment Article dan upload post ke firebase
         val _btnArticle = view.findViewById<Button>(R.id.btnPost)
+        _etTitle = view.findViewById<EditText>(R.id.etTitle)
+        _etDesc = view.findViewById<EditText>(R.id.etDesc)
+        _etAuthor = view.findViewById<EditText>(R.id.etAuthor)
+        _etImage = view.findViewById<EditText>(R.id.etImage)
+
         _btnArticle.setOnClickListener{
+            TambahData(
+                _etTitle.text.toString(),
+                _etDesc.text.toString(),
+                _etAuthor.text.toString(),
+                _etImage.text.toString()
+            )
             val mArticle = Article()
             val mFragmentManager : FragmentManager = parentFragmentManager
             mFragmentManager.beginTransaction().apply {
@@ -48,6 +64,61 @@ class PostArticle : Fragment() {
                 commit()
             }
         }
+    }
+
+//    Firebase
+//
+    fun TambahData(Title : String, Description : String, Author: String, Image: String){
+        var View = Random.nextInt(20,900)
+        val dataBaru = ArticleData(Title, Description, Author, Image, View)
+        db.collection("article")
+            .document(Title)
+            .set(dataBaru)
+            .addOnSuccessListener {
+                _etTitle.setText("")
+                _etDesc.setText("")
+                Toast.makeText(
+                    requireContext(), // Use requireContext() here
+                    "Data Berhasil disimpan",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            .addOnFailureListener { e ->
+                Log.w(
+                    "PROJ_DMFIREBASE",
+                    "Error adding document",
+                    e
+                )
+            }
+    }
+
+    fun ReadData(){
+        db.collection("article")
+            .get()
+            .addOnSuccessListener {
+                    result ->
+                dataArticle.clear()
+                for (document in result){
+                    val hasil = ArticleData (
+                        document.data.get("title").toString(),
+                        document.data.get("description").toString(),
+                        document.data.get("author").toString(),
+                        document.data.get("image").toString(),
+                        document.data.get("view") as Int
+                    )
+                    dataArticle.add(hasil)
+                }
+            }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+
 
     }
 
